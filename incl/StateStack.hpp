f68797c3abc5e674c2d3c18a6030968aba7b955d
MickeMakaron <mikael.hernvall@gmail.com>
2015-03-02 14:40:45 +0100

    Stack for program states.

---
+++ incl/StateStack.hpp
@@ -0,0 +1,123 @@
+/****************************************************************
+****************************************************************
+*
+* Territorial - 2D RTS game with dynamic territorial borders.
+* Copyright (C) 2015 Mikael Hernvall (mikael.hernvall@gmail.com)
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+****************************************************************
+****************************************************************/
+
+#ifndef ANTGAME_STATESTACK_HPP
+#define ANTGAME_STATESTACK_HPP
+
+//#include <State.hpp>
+#include "SFML/Window/Event.hpp"
+namespace State
+{
+    typedef int* Ptr;
+}
+
+////////////////////////////////////////////////
+// C++ Standard Library
+#include <list>
+#include <utility>
+#include <functional>
+#include <map>
+////////////////////////////////////////////////
+
+////////////////////////////////////////////////
+// SFML - Simple and Fast Media Library
+#include "SFML/System/NonCopyable.hpp"
+////////////////////////////////////////////////
+
+
+class StateStack : private sf::NonCopyable
+{
+    public:
+        enum Action
+        {
+            Push,
+            Pop,
+            Clear,
+        };
+
+    public:
+        /**
+         * \brief Constructor
+         */
+        StateStack();
+
+        /**
+         * \brief Update states.
+         */
+        void update();
+
+        /**
+         * \brief Draw states.
+         */
+        void draw();
+
+        /**
+         * \brief Handle input events.
+         *
+         * \param event event to handle.
+         */
+        void handleEvent(const sf::Event& event);
+
+        /**
+         * \brief Push state on top of stack.
+         *
+         * Note that the unique State pointer will
+         * be MOVED to the stack, resulting in
+         * invalidation.
+         *
+         * \param state unique State pointer to push.
+         */
+        void push(State::Ptr& state);
+
+        /**
+         * \brief Pop state on top of stack.
+         */
+        void pop();
+
+        /**
+         * \brief Clear all states from stack.
+         */
+        void clear();
+
+        /**
+         * \brief Check if stack is empty.
+         */
+        bool isEmpty() const;
+
+    private:
+        void                applyPendingChanges();
+
+    private:
+        std::list<State::Ptr>     mStack;       ///< FILO-stack of unique State pointers
+        std::list<State::Ptr>     mPushList;    ///< FIFO-list of pending state pushes.
+        std::list<Action>         mPendingList; ///< FIFO-list of pending pop and clear requests.
+};
+
+
+/************************************************
+ * \class StateStack
+ *
+ * FILO-stack for program states.
+ *
+************************************************/
+
+#endif // ANTGAME_STATESTACK_HPP
---
+++ src/StateStack.cpp
@@ -0,0 +1,116 @@
+/****************************************************************
+****************************************************************
+*
+* Territorial - 2D RTS game with dynamic territorial borders.
+* Copyright (C) 2015 Mikael Hernvall (mikael.hernvall@gmail.com)
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+****************************************************************
+****************************************************************/
+
+#include <StateStack.hpp>
+
+////////////////////////////////////////////////
+// C++ Standard Library
+#include <cassert>
+////////////////////////////////////////////////
+
+StateStack::StateStack()
+{
+}
+
+void StateStack::update()
+{
+    /*
+     * Update from top of stack to bottom. Stop if a state's update
+     * function returns false.
+     */
+    for(auto it = mStack.rbegin(); it != mStack.rend(); it++)
+        if(!(*it)->update())
+            break;
+
+    // Only apply changes after all states have been updated.
+    applyPendingChanges();
+}
+
+
+
+void StateStack::draw()
+{
+    // Draw all active states from bottom to top
+    for(State::Ptr& state : mStack)
+        state->draw();
+}
+
+void StateStack::push(State::Ptr& state)
+{
+    mPushList.push_back(std::move(state));
+    mPendingList.push_back(Push);
+}
+
+void StateStack::pop()
+{
+    mPendingList.push_back(Pop);
+}
+
+void StateStack::clear()
+{
+    mPendingList.push_back(Clear);
+}
+
+
+bool StateStack::isEmpty() const
+{
+    return mStack.empty();
+}
+
+
+void StateStack::handleEvent(const sf::Event& event)
+{
+    /*
+     * Update from top of stack to bottom. Stop if a state's event
+     * handler returns false.
+     */
+    for(auto it = mStack.rbegin(); it != mStack.rend(); it++)
+        if(!(*it)->handleEvent(event))
+            break;
+
+    // Only apply changes after all states have handled the events.
+    applyPendingChanges();
+}
+
+void StateStack::applyPendingChanges()
+{
+    for(Action action : mPendingList)
+    {
+        switch(action)
+        {
+            case Push:
+                mStack.push_back(std::move(mPushList.front()));
+                mPushList.pop_front();
+                break;
+
+            case Pop:
+                mStack.pop_back();
+                break;
+
+            case Clear:
+                mStack.clear();
+                break;
+        }
+    }
+
+    mPendingList.clear();
+}
