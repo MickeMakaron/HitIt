f8f3374bf1b9957939db8239d9e7c843c6700d8a
MickeMakaron <mikael.hernvall@gmail.com>
2015-03-02 14:42:01 +0100

    Entry point to program. Contains main program loop.

---
+++ incl/HitIt.hpp
@@ -0,0 +1,79 @@
+/****************************************************************
+****************************************************************
+*
+* HitIt - Top-down 2D game where the goal is NOT to hit the music.
+* Copyright (C) 2015 Mikael Hernvall (mikael.hernvall@gmail.com)
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+****************************************************************
+****************************************************************/
+
+#ifndef HITIT_HITIT_HPP
+#define HITIT_HITIT_HPP
+
+////////////////////////////////////////////////
+// SFML - Simple and Fast Media Library
+#include "SFML/Graphics/RenderWindow.hpp"
+#include "SFML/Window/Event.hpp"
+////////////////////////////////////////////////
+
+
+#include "StateStack.hpp"
+
+class HitIt
+{
+    public:
+        /**
+         * \brief Constructor
+         *
+         * \param sizeX width of window in pixels.
+         * \param sizeY height of window in pixels.
+         */
+        HitIt(unsigned int sizeX, unsigned int sizeY);
+
+        /**
+         * \brief Run main loop
+         */
+        void run();
+
+    private:
+        /**
+         * \brief Process input from input devices.
+         */
+        void processInput();
+
+
+
+        /**
+         * \brief Clear, draw and display program graphics.
+         */
+        void render();
+
+    private:
+        sf::RenderWindow mWindow;    ///< SFML window object.
+        StateStack mStateStack;      ///< State stack for program states.
+};
+
+
+/************************************************
+ * \class HitIt
+ *
+ * Main application loop. To run the program, declare
+ * a HitIt object and call its run function.
+ *
+************************************************/
+
+
+#endif // HITIT_HITIT_HPP
---
+++ src/HitIt.cpp
@@ -0,0 +1,82 @@
+/****************************************************************
+****************************************************************
+*
+* HitIt - Top-down 2D game where the goal is NOT to hit the music.
+* Copyright (C) 2015 Mikael Hernvall (mikael.hernvall@gmail.com)
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+****************************************************************
+****************************************************************/
+
+#include "HitIt.hpp"
+//#include "TIME_PER_FRAME.hpp"
+
+#include <sstream>
+
+HitIt::HitIt(unsigned int sizeX, unsigned int sizeY)
+: mWindow(sf::VideoMode(sizeX, sizeY), "Hit it!", sf::Style::Titlebar | sf::Style::Close)
+{
+    mWindow.setMouseCursorVisible(false);
+    //TIME_PER_FRAME::setAsSeconds(1/60.f);
+}
+
+void HitIt::processInput()
+{
+    sf::Event event;
+    while(mWindow.pollEvent(event))
+    {
+        mStateStack.handleEvent(event);
+
+        if(event.type == sf::Event::Closed)
+            mWindow.close();
+    }
+}
+
+void HitIt::render()
+{
+    mWindow.clear();
+    mStateStack.draw();
+    mWindow.display();
+}
+
+void HitIt::run()
+{
+    sf::Clock clock;
+    sf::Time timeSinceLastUpdate = sf::Time::Zero;
+   // sf::Time timePerFrame = sf::Time(sf::seconds(TIME_PER_FRAME::S));
+    sf::Time timePerFrame = sf::Time(sf::seconds(1/60.f));
+    while(mWindow.isOpen())
+    {
+        sf::Time dt = clock.restart();
+        timeSinceLastUpdate += dt;
+        while (timeSinceLastUpdate > timePerFrame)
+        {
+            timeSinceLastUpdate -= timePerFrame;
+            processInput();
+            mStateStack.update();
+
+            if(mStateStack.isEmpty())
+                mWindow.close();
+        }
+        render();
+
+        ////////////////////////////////////////////////
+        // DEBUG - FPS COUNTER
+        std::ostringstream stream;
+        stream << "Hit it! | FPS: " << 1 / dt.asSeconds();
+        mWindow.setTitle(stream.str());
+        ////////////////////////////////////////////////
+    }
+}
