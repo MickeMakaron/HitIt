085f8b55f9fee67d07b74fa2fd889cac7535a630
MickeMakaron <mikael.hernvall@gmail.com>
2015-03-04 16:43:29 +0100

    Player world object.

---
+++ incl/Player.hpp
@@ -0,0 +1,54 @@
+/****************************************************************
+****************************************************************
+*
+* HitIt - Top-down 2D game where the goal is NOT to hit the music.
+* Copyright (C) 2015 Mikael Hernvall (mikael.hernvall@gmail.com)
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+****************************************************************
+****************************************************************/
+
+#ifndef HITIT_PLAYER_HPP
+#define HITIT_PLAYER_HPP
+
+////////////////////////////////////////////////
+// HitIt internal headers
+#include "SpriteNode.hpp"
+////////////////////////////////////////////////
+
+
+class Player : public SpriteNode
+{
+    public:
+        Player(const sf::Texture& texture, int hp, sf::Vector2f position = sf::Vector2f(0.f, 0.f));
+
+        virtual bool            isMarkedForRemoval() const;
+
+        void damage(int points);
+        void destroy();
+        bool isDestroyed() const;
+        void accelerate(float a);
+
+    private:
+        virtual void drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const;
+        virtual void updateCurrent();
+
+    private:
+        int             mHp;
+        float           mMovementSpeed;
+        float           mDiagonalMovementSpeed;
+};
+
+#endif // HITIT_PLAYER_HPP
---
+++ src/Player.cpp
@@ -0,0 +1,97 @@
+/****************************************************************
+****************************************************************
+*
+* HitIt - Top-down 2D game where the goal is NOT to hit the music.
+* Copyright (C) 2015 Mikael Hernvall (mikael.hernvall@gmail.com)
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+****************************************************************
+****************************************************************/
+
+
+
+
+////////////////////////////////////////////////
+// SFML - Simple and Fast Media Library
+#include "SFML/Graphics/RenderTarget.hpp"
+#include "SFML/Window/Keyboard.hpp"
+////////////////////////////////////////////////
+
+////////////////////////////////////////////////
+// HitIt internal headers
+#include "Player.hpp"
+#include "TIME_PER_FRAME.hpp"
+////////////////////////////////////////////////
+
+
+Player::Player(const sf::Texture& texture, int hp, sf::Vector2f position)
+: SpriteNode(texture)
+, mHp(hp)
+, mMovementSpeed(200.f)
+, mDiagonalMovementSpeed(sqrtf(0.5f * mMovementSpeed * 0.5f * mMovementSpeed * 2))
+{
+    setPosition(position);
+}
+
+
+void Player::updateCurrent()
+{
+    using namespace sf;
+
+    Vector2f direction(0.f, 0.f);
+    if(Keyboard::isKeyPressed(Keyboard::Left))
+        direction.x = -1.f;
+    else if(Keyboard::isKeyPressed(Keyboard::Right))
+        direction.x = 1.f;
+
+    if(Keyboard::isKeyPressed(Keyboard::Up))
+        direction.y = -1.f;
+    else if(Keyboard::isKeyPressed(Keyboard::Down))
+        direction.y = 1.f;
+
+    float velocity = direction.x && direction.y ? mDiagonalMovementSpeed : mMovementSpeed;
+    move(direction * velocity * TIME_PER_FRAME::seconds());
+}
+
+void Player::drawCurrent(sf::RenderTarget& target, sf::RenderStates states) const
+{
+    SpriteNode::drawCurrent(target, states);
+    drawBoundingRect(target, states);
+}
+
+bool  Player::isDestroyed() const
+{
+    return mHp <= 0;
+}
+
+void Player::damage(int points)
+{
+    mHp -= points;
+}
+
+void Player::destroy()
+{
+    mHp = 0;
+}
+
+void Player::accelerate(float a)
+{
+    mMovementSpeed += a;
+}
+
+bool Player::isMarkedForRemoval() const
+{
+    return isDestroyed();
+}
