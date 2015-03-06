73631dfe93c9ed2ff119cf450bd12fb1b9e97234
MickeMakaron <mikael.hernvall@gmail.com>
2015-03-06 13:18:38 +0100

    New assets

--- incl/ResourceIdentifiers.hpp
+++ incl/ResourceIdentifiers.hpp
@@ -33,6 +33,9 @@ namespace ResourceID
             MenuStateBg,
             LoadingStateBg,
             Player,
+            AudienceTerrace,
+            Fence,
+            WorldBg,
             IDCount,
         };
     }
