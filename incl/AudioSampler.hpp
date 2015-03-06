7218b8ca29c2d5c9154c0d39068a3aaf4948157f
MickeMakaron <mikael.hernvall@gmail.com>
2015-03-06 22:13:28 +0100

    New class: AudioSampler * Creates SFML sound buffer objects for a range of tones using raw audio data.

---
+++ incl/AudioSampler.hpp
@@ -0,0 +1,61 @@
+/****************************************************************
+****************************************************************
+*
+* HitIt - Top-down 2D game where the goal is NOT to hit the music.
+* Copyright (C) 2015 Mikael Hernvall (mikael.hernvall@gmail.com)
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+****************************************************************
+****************************************************************/
+
+#ifndef HITIT_AUDIOSAMPLER_HPP
+#define HITIT_AUDIOSAMPLER_HPP
+
+////////////////////////////////////////////////
+// C++ Standard Library
+#include <vector>
+////////////////////////////////////////////////
+
+////////////////////////////////////////////////
+// SFML - Simple and Fast Media Library
+#include "SFML/Audio/SoundBuffer.hpp"
+////////////////////////////////////////////////
+
+
+class AudioSampler
+{
+    public:
+        AudioSampler(unsigned int sampleRate = 44100, double volume = 2000);
+
+        sf::SoundBuffer& getBuffer(unsigned int iTone);
+
+    private:
+        void createSamples(sf::Int16*& samples, unsigned int tone, unsigned int& numSamples, unsigned int sampleRate, double volume) const;
+
+    private:
+        const double BOTTOM_C = 57.;
+        const double TWO_PI = 6.283185307179586476925286766559;
+        std::vector<sf::SoundBuffer> mBuffers;
+};
+
+
+/************************************************
+ * \class AudioSampler
+ *
+ * Create raw audio samples for all tones.
+ *
+************************************************/
+
+#endif // HITIT_AUDIOSAMPLER_HPP
---
+++ src/AudioSampler.cpp
@@ -0,0 +1,47 @@
+////////////////////////////////////////////////
+// C++ Standard Library
+#include <stdexcept>
+#include <cmath>
+#include <cstring>
+////////////////////////////////////////////////
+
+////////////////////////////////////////////////
+// HitIt internal headers
+#include "AudioSampler.hpp"
+////////////////////////////////////////////////
+AudioSampler::AudioSampler(unsigned int sampleRate, double volume)
+: mBuffers(BOTTOM_C * 2)
+{
+    for(unsigned int iTone = 0; iTone < mBuffers.size(); iTone++)
+    {
+        sf::Int16* samples;
+        unsigned int numSamples;
+        createSamples(samples, iTone, numSamples, sampleRate, volume);
+
+        if(!mBuffers[iTone].loadFromSamples(samples, numSamples, 1, sampleRate))
+            std::runtime_error("AudioSampler::AudioSampler - Failed to load samples of tone " + iTone);
+    }
+}
+
+
+void AudioSampler::createSamples(sf::Int16*& samples, unsigned int tone, unsigned int& numSamples, unsigned int sampleRate, double volume) const
+{
+    const double FREQUENCY = 440. * pow(2, ((double)(tone - BOTTOM_C))/ 12.);
+    const double INCREMENT = TWO_PI * FREQUENCY / sampleRate;
+    numSamples = sampleRate / FREQUENCY;
+
+    sf::Int16* derp = new sf::Int16[numSamples];
+    double time = 0;
+    for(unsigned int i = 0; i < numSamples; i++)
+    {
+        derp[i] = volume * sin(time);
+        time += INCREMENT;
+    }
+
+    samples = derp;
+}
+
+sf::SoundBuffer& AudioSampler::getBuffer(unsigned int iTone)
+{
+    return mBuffers[iTone];
+}
