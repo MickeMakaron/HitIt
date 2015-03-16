b202e95d1cd9be6b6641e60d2f364543135e5095
MickeMakaron <mikael.hernvall@gmail.com>
2015-03-16 21:53:03 +0100

    Distilled logic for playing and displaying the theme in the background in the menus from MainMenu to a separate state class operating behind the other menu states.

---
+++ incl/MenuThemeState.hpp
@@ -0,0 +1,100 @@
+/****************************************************************
+****************************************************************
+*
+* HitIt - Top-down 2D game where the goal is NOT to hit the music.
+* Copyright (C) 2015 Mikael Hernvall (mikael.hernvall@gmail.com)
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+****************************************************************
+****************************************************************/
+
+#ifndef HITIT_MENUTHEMESTATE_HPP
+#define HITIT_MENUTHEMESTATE_HPP
+
+////////////////////////////////////////////////
+// SFML - Simple and Fast Media Library
+#include "SFML/Graphics/RectangleShape.hpp"
+////////////////////////////////////////////////
+
+////////////////////////////////////////////////
+// HitIt internal headers
+#include "State.hpp"
+#include "AudioSampler.hpp"
+#include "SceneNode.hpp"
+#include "AssetList.hpp"
+class Spawner;
+////////////////////////////////////////////////
+
+class MenuThemeState : public State
+{
+	public:
+        /**
+         * \brief Constructor
+         *
+         * \param stack to insert state into at construction.
+         * \param target SFML RenderTarget object to draw to.
+         */
+        MenuThemeState(StateStack& stack, sf::RenderTarget& target);
+        ~MenuThemeState();
+
+        /**
+         * \brief Draw state
+         */
+		virtual void draw();
+
+        /**
+         * \brief Update state
+         *
+         * \return indicate whether the state stack
+         * should continue updating further states
+         * (true) or not (false).
+         */
+		virtual bool update();
+
+        /**
+         * \brief Handle input events.
+         *
+         * \param event event to handle
+         *
+         * \return indicate whether the state stack
+         * should allow further states to handle events
+         * (true) or not (false).
+         */
+        virtual bool handleEvent(const sf::Event& event);
+
+    private:
+        std::list<TextureList::Asset> getTextures() const;
+
+
+    protected:
+        const std::string       M_THEME_FILE_PATH = "assets/midi/damp.mid";
+        TextureList             mTextures;
+        sf::RenderTarget&       mTarget;
+        AudioSampler            mSampler;
+        SceneNode               mNotes;
+        Spawner*                mSpawner1;
+        Spawner*                mSpawner2;
+        sf::RectangleShape      mBackground;
+};
+
+/************************************************
+ * \class MenuThemeState
+ *
+ * Program state for playing and displaying the theme
+ * behind the menus.
+ *
+************************************************/
+
+#endif // HITIT_MENUTHEMESTATE_HPP
---
+++ src/MenuThemeState.cpp
@@ -0,0 +1,118 @@
+/****************************************************************
+****************************************************************
+*
+* HitIt - Top-down 2D game where the goal is NOT to hit the music.
+* Copyright (C) 2015 Mikael Hernvall (mikael.hernvall@gmail.com)
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+****************************************************************
+****************************************************************/
+
+
+////////////////////////////////////////////////
+// SFML - Simple and Fast Media Library
+#include "SFML/Window/Event.hpp"
+#include "SFML/Graphics/RenderTarget.hpp"
+////////////////////////////////////////////////
+
+////////////////////////////////////////////////
+// HitIt internal headers
+#include "MenuThemeState.hpp"
+#include "Spawner.hpp"
+#include "VertexArrayNode.hpp"
+////////////////////////////////////////////////
+
+
+MenuThemeState::MenuThemeState(StateStack& stack, sf::RenderTarget& target)
+: State(stack)
+, mTarget(target)
+, mSpawner1(new Spawner(M_THEME_FILE_PATH, mSampler, sf::FloatRect(sf::Vector2f(0.f, 0.f), mTarget.getView().getSize()), &mNotes))
+, mSpawner2(nullptr)
+, mBackground(mTarget.getView().getSize())
+{
+    mTextures.insert(getTextures());
+    mSampler.setVolume(60.f);
+
+    Assets::get(ResourceID::Texture::MenuStateBg).setRepeated(true);
+    mBackground.setTexture(&Assets::get(ResourceID::Texture::MenuStateBg));
+    mBackground.setTextureRect(sf::IntRect(0, 0, mTarget.getView().getSize().x, mTarget.getView().getSize().y));
+}
+
+////////////////////////////////////////////////
+
+MenuThemeState::~MenuThemeState()
+{
+    if(mSpawner1)
+        delete mSpawner1;
+    if(mSpawner2)
+        delete mSpawner2;
+}
+
+void MenuThemeState::draw()
+{
+    mTarget.draw(mBackground);
+    mTarget.draw(mNotes);
+}
+
+bool MenuThemeState::update()
+{
+    mNotes.update();
+
+    mSpawner1->update();
+    mSpawner1->spawn();
+
+    if(mSpawner2)
+    {
+        mSpawner2->update();
+        mSpawner2->spawn();
+    }
+
+    mNotes.removeWrecks();
+
+
+    if(mSpawner2)
+    {
+        if(mSpawner1->getObstacles().getSize() == 0)
+        {
+            mSampler.pause();
+            delete mSpawner1;
+            mSampler.resume();
+
+            mSpawner1 = mSpawner2;
+
+            mSpawner2 = nullptr;
+        }
+    }
+    else if(mSpawner1->isEmpty())
+    {
+        mSpawner2 = new Spawner(M_THEME_FILE_PATH, mSampler, sf::FloatRect(sf::Vector2f(0.f, 0.f), mTarget.getView().getSize()), &mNotes);
+    }
+
+    return true;
+}
+
+bool MenuThemeState::handleEvent(const sf::Event& event)
+{
+    return true;
+}
+
+std::list<TextureList::Asset> MenuThemeState::getTextures() const
+{
+    namespace ID = ResourceID::Texture;
+    return
+    {
+        TextureList::Asset(ID::MenuStateBg,  "textures/menu_bg.png"),
+    };
+}
